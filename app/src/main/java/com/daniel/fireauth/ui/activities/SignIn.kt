09f998151d2032a985baed2f56c2e83780d8c414
Daniel011 <cariasdaniel261@gmail.com>
2023-02-07 10:16:02 -0600

    style: Login and registration interface design

--- app/src/main/java/com/daniel/fireauth/ui/activities/SignIn.kt
+++ app/src/main/java/com/daniel/fireauth/ui/activities/SignIn.kt
@@ -1,12 +1,29 @@
 package com.daniel.fireauth.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import com.daniel.fireauth.R
+import com.daniel.fireauth.databinding.ActivitySignInBinding
 
 class SignIn : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+
+    }
+
+    private fun initUI(){
+        binding.signUpButton.setOnClickListener {
+            startActivity(Intent(this, SignUp::class.java))
+            Animatoo.animateSlideLeft(this)
+        }
     }
 }
--- app/src/main/java/com/daniel/fireauth/ui/activities/SignUp.kt
+++ app/src/main/java/com/daniel/fireauth/ui/activities/SignUp.kt
@@ -2,11 +2,36 @@ package com.daniel.fireauth.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.daniel.fireauth.R
+import androidx.activity.OnBackPressedCallback
+import com.blogspot.atifsoftwares.animatoolib.Animatoo
+import com.daniel.fireauth.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+
+    }
+
+    private fun initUI(){
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+        binding.backLayout.setOnClickListener { onBack() }
+    }
+
+    private val onBackPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(true){
+        override fun handleOnBackPressed() {
+            onBack()
+        }
+    }
+
+    private fun onBack(){
+        finish()
+        Animatoo.animateSlideRight(this)
     }
 }
